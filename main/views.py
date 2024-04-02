003ac53f25b627058eafda68f9b0f05132c5471c
RomanMainAcc <romanborisuk0@gmail.com>
2024-04-02 17:57:27 +0300

    the main app is complete

--- main/views.py
+++ main/views.py
@@ -3,8 +3,17 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, )
+    context = {
+        "title": "CraftedHaven - home page",
+        "content": "Furniture store CraftedHaven",
+    }
+    return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse("About")
+    context = {
+        "title": "CraftedHaven - about us",
+        "content": "About us",
+        "text_on_page": "Our furniture is made using the finest materials and meets the highest quality standards."
+    }
+    return render(request, 'main/about.html', context)
