84cd4dd5863a244ca91c509bc9cb3742908b7c1d
RomanMainAcc <romanborisuk0@gmail.com>
2024-04-13 18:20:10 +0300

    add to cart to view

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from goods.models import Products
+from users.models import User
+
+
+class CartQuerySet(models.QuerySet):
+    def total_price(self):
+        return sum(cart.products_price() for cart in self.all())
+
+    def total_quantity(self):
+        if self:
+            sum(cart.quantity for cart in self.all())
+        return 0
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    session_key = models.CharField(max_length=32, blank=True, null=True)
+
+    objects = CartQuerySet().as_manager()
+
+    def products_price(self):
+        return round(self.product.sell_price() * self.quantity, 2)
+
+    def __str__(self):
+        return f"Cart {self.user.username} | Product {self.product.name} | Quantity {self.quantity}"
