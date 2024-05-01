fc7aee2fa23adc5bb4db18ccf9881fd628cc1a1e
RomanMainAcc <romanborisuk0@gmail.com>
2024-05-01 15:52:29 +0300

    add jwt api auth

--- CraftedHaven/urls.py
+++ CraftedHaven/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from CraftedHaven import settings
 from api.views import CategoryViewSet, ProductsViewSet
@@ -30,6 +31,9 @@ router.register(r'products', ProductsViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('', include("main.urls", namespace="main")),
     path('catalog/', include("goods.urls", namespace="catalog")),
     path('user/', include("users.urls", namespace="user")),
