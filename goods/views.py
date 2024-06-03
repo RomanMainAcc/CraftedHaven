e5a39e0972dd4786ef9e7d35bd7a3465fe297690
RomanMainAcc <romanborisuk0@gmail.com>
2024-06-03 20:48:45 +0300

    add filter: products liked by current user

--- goods/views.py
+++ goods/views.py
@@ -2,19 +2,23 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.views import View
 from django.http import JsonResponse
+from django.views.generic import TemplateView
 
 from goods.models import Products, LikedProduct
 from goods.utils import q_search
 
 
-class CatalogView(View):
+class CatalogView(TemplateView):
     template_name = "goods/catalog.html"
 
-    def get(self, request, category_slug=None):
-        page_number = request.GET.get('page', 1)
-        on_sale = request.GET.get('on_sale', None)
-        order_by = request.GET.get('order_by', None)
-        query = request.GET.get('q', None)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_slug = self.kwargs.get('category_slug')
+        page_number = self.request.GET.get('page', 1)
+        on_sale = self.request.GET.get('on_sale', None)
+        order_by = self.request.GET.get('order_by', None)
+        query = self.request.GET.get('q', None)
+        liked_only = self.request.GET.get('liked_only', None)
 
         if category_slug == 'all':
             goods = Products.objects.all()
@@ -29,30 +33,35 @@ class CatalogView(View):
         if order_by and order_by != 'default':
             goods = goods.order_by(order_by)
 
+        if liked_only:
+            goods = Products.objects.filter(likedproduct__user=self.request.user)
+
         paginator = Paginator(goods, 3)
         current_page = paginator.page(int(page_number))
 
         for product in current_page:
             product.likes = product.get_likes()
 
-        context = {
+        context.update({
             "title": "Catalog",
             "goods": current_page,
             "slug_url": category_slug,
-        }
-        return render(request, self.template_name, context)
+        })
+        return context
 
 
-class ProductView(View):
+class ProductView(TemplateView):
     template_name = "goods/product.html"
 
-    def get(self, request, product_slug):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product_slug = self.kwargs.get('product_slug')
         product_obj = get_object_or_404(Products, slug=product_slug)
         product_obj.likes = product_obj.get_likes()
-        context = {
+        context.update({
             "product": product_obj,
-        }
-        return render(request, self.template_name, context)
+        })
+        return context
 
 
 class LikeView(View):
